6e5241b5a261e061196cf3d9dfd0fd1906f6d92f
inno <innocent.ongeri@student.moringaschool.com>
2022-09-06 10:23:19 +0300

    introduced delete and read buttons

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -2,15 +2,25 @@ import React, { useState } from 'react';
 
 const poemAPI = "https://poetrydb.org/title/lines";
 
-function Poem({poem}) {
+function Poem({poem, removePoem, addToFavorites}) {
     const {title, lines, author} = poem;
-    
+    const [isRead, setIsRead] = useState(false)
+    function onDeleteClick(e) {
+      e.preventDefault();
+      fetch(`${poemAPI}/${poem.id}`, {
+        method: "DELETE",
+      });
+      removePoem(poem);
+    }
   
     return (
       <div>
         <h3>{title}</h3>
         <p>{lines}</p>
         <p><strong>- By {author}</strong></p>
+        <button onClick={() => setIsRead(!isRead)} > Mark as {isRead ? "unread" : "read" }</button>
+        <button onClick={onDeleteClick} > Delete</button>
+        <button onClick={() => addToFavorites(poem)}> {poem.isFavorite ? "Unfavorite" : "⭐Favorite" }</button>
       </div>
     );
   }
