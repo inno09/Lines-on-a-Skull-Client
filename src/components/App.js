75e24b597dbd2fad920f3064585930f1ab83dd8e
inno <innocent.ongeri@student.moringaschool.com>
2022-09-07 11:42:25 +0300

    created sidebar

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
+import Sidebar from './Sidebar';
+// import Home from './Home';
+// import Search from './Search';
+// import Authors from './Authors';
+// import Genre from './Genre';
+// import Favorites from './Favorites';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      
     </div>
   );
 }
