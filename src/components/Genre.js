d8389381e48a59afdb1514689ad2fd7a19bb47e6
inno <innocent.ongeri@student.moringaschool.com>
2022-09-09 06:25:10 +0300

    created state

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function Genre() {
+
+  const [genres, setGenre] = useState([]);
+
   return <div>Genre</div>;
 }
 
 export default Genre;
+
